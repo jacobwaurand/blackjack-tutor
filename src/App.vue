ef77fdef94ee13c608c3f6682ffd56961328a379
Jacob Aurand <jacobwaurand@gmail.com>
2026-02-28 00:20:31 -0500

    Start Blackjack store

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import DeckCard from './components/DeckCard.vue'
 
 <template>
   <div class="flex h-screen w-3/5 flex-col justify-center">
-    <div class="bg-dark-bg flex h-1/2 flex-col justify-between rounded-2xl px-[3vw] py-[3vh]">
+    <div class="bg-dark-bg flex min-h-1/2 flex-col justify-between rounded-2xl px-[3vw] py-[3vh]">
       <div class="flex justify-center"><DeckCard /><DeckCard /></div>
       <div class="flex justify-center"><DeckCard /><DeckCard /></div>
     </div>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,39 @@
-import { ref, computed } from 'vue'
+import { ref, computed, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+interface Rank { text: string, value: number | number[]}
+interface Card { suit: string, rank: Rank}
+
+export const useBlackjackStore = defineStore('blackjack', () => {
+  const deck = ref([])
+  const numOfDecks = ref(8)
+  const suits = ref(['heart', 'spade', 'diamond', 'club'])
+  const ranks: Ref<Rank[]> = ref([
+    {text: '1', value: 1},
+    {text: '2', value: 2},
+    {text: '3', value: 4},
+    {text: '4', value: 4},
+    {text: '5', value: 5},
+    {text: '6', value: 6},
+    {text: '7', value: 7},
+    {text: '8', value: 8},
+    {text: '9', value: 9},
+    {text: '10', value: 10},
+    {text: 'J', value: 10},
+    {text: 'Q', value: 10},
+    {text: 'K', value: 10},
+    {text: 'A', value: [1, 11]}
+  ])
+
+  function initializeDeck() {
+    for(let i = 0; i < numOfDecks.value; i++) {
+      for(const suit of suits.value) {
+        for(const rank of ranks.value) {
+          deck.value.push()
+        }
+      }
+    }
   }
 
-  return { count, doubleCount, increment }
+  return { deck, initializeDeck }
 })
